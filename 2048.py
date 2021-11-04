909e068264044d860f5b1c6283631f5a2d0c934a
Nicholas Jefferis <jefferns@mail.uc.edu>
2021-11-03 20:53:25 -0400

    tile movement

--- 2048.py
+++ 2048.py
@@ -6,6 +6,7 @@ pygame.init()
 
 HEIGHT, WIDTH = 600,600
 T_HEIGHT, T_WIDTH = HEIGHT//4, WIDTH//4
+T_DIM = 4
 
 surface = pygame.display.set_mode((HEIGHT,WIDTH),0,32)  # width, height, flags, depth (bits per pixel)
 pygame.display.set_caption("2048")   # Title on the popup window
@@ -37,6 +38,65 @@ colors = {
 }
 
 ####################################################
+#                   Summation                      #
+####################################################
+def mergetiles():
+    for i in range(T_DIM):
+        for k in range(T_DIM):
+            if tiles[i][k] == tiles[i][k+1] and tiles[i][k] != 0:
+                tiles[i][k] = tiles[i][k]*2
+                tiles[i][k+1] = 0 
+                score += tiles[i][k]
+                moveRight()
+
+
+####################################################
+#                   Move Tiles                     #
+####################################################
+def moveRight():
+    for x in range(T_DIM-2,-1,-1):
+        for y in range(T_DIM):
+            for z in range(T_DIM-1-x):
+                if not tiles[x+z+1][y]:
+                    tiles[x+z+1][y] = tiles[x+z][y]
+                    tiles[x+z][y] = 0
+def moveLeft():
+    for x in range(1,T_DIM):
+        for y in range(T_DIM):
+            for z in range(x):
+                if not tiles[x-1-z][y] and tiles[x-z][y]:
+                    tiles[x-z-1][y] = tiles[x-z][y]
+                    tiles[x-z][y] = 0
+def moveUp():
+    for x in range(T_DIM):
+        for y in range(1,T_DIM):
+            for z in range(y):
+                if not tiles[x][y-1-z] and tiles[x][y-z]:
+                    tiles[x][y-1-z] = tiles[x][y-z]
+                    tiles[x][y-z] = 0
+def moveDown():
+    for x in range(T_DIM):
+        for y in range(T_DIM-2,-1,-1):
+            for z in range(T_DIM-1-y):
+                if not tiles[x][y+1+z]:
+                    tiles[x][y+1+z] = tiles[x][y+z]
+                    tiles[x][y+z] = 0
+
+            
+####################################################
+#                  Evaluate Move                   #
+####################################################
+# def canMove():
+#     for x in range(T_DIM):
+#         for y in range(T_DIM):
+#             if tiles[x][y-1] == 0 and tiles[x][y] > 0:
+#                 return True 
+#             elif (tiles[x][y-1] == tiles[x][y]) and tiles[x][y-1] != 0:
+#                 return True
+#     return False
+
+
+####################################################
 #                   Random Tile                    #
 ####################################################
 def addRandomTile():
@@ -54,8 +114,8 @@ def printBoard():
     border = 2
     surface.fill((240,240,240))
 
-    for i in range(len(tiles)):
-        for j in range(len(tiles[i])):
+    for i in range(T_DIM):
+        for j in range(T_DIM):
             # Draw a rectangle on 'surface' with color based on tile value
             # Third Arg: (top left x, top left y, width, height)
             pygame.draw.rect(surface, colors[tiles[i][j]], (i*T_HEIGHT+i*border, j*T_WIDTH+j*border, T_HEIGHT-2*border, T_WIDTH-2*border))
@@ -74,6 +134,7 @@ def printBoard():
 ####################################################
 addRandomTile()
 addRandomTile()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -81,18 +142,26 @@ while True:
             pygame.quit()
             sys.exit()
         
+        if not canMove():
+            print("you lost")
+            pygame.quit()
+            sys.exit()
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                # if valid_move('right'):
-                #     movetiles('right')
+                moveRight()
+                lost = canMove()
                 print('right')
             elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                moveLeft()
                 print('left')
             elif event.key == pygame.K_UP or event.key == pygame.K_w:
+                moveUp()
                 print('up')
             elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                moveDown()
                 print('down')
+            addRandomTile()
 
-
-    printBoard()
-    pygame.display.update()
+        printBoard()
+        pygame.display.update()
