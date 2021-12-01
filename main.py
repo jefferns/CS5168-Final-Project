54f786eb9f03875f0f2527976fd32123e80462c2
Nicholas Jefferis <jefferns@mail.uc.edu>
2021-12-01 18:36:13 -0500

    test

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import os
 import gym_2048
 import gym
 import numpy as np
 from actor_critic import Agent
 from gym import wrappers
 import matplotlib.pyplot as plt
+
+
+CHILD_COUNT = 10
+GAME_COUNT = 1000
+
 
 def plot_learning_curve(x, scores, figure_file):
     running_avg = np.zeros(len(scores))
@@ -13,18 +19,25 @@ def plot_learning_curve(x, scores, figure_file):
     plt.title('Running average of previous 100 scores')
     plt.savefig(figure_file)
 
-if __name__ == '__main__':
+pid_list = []
+for _ in range(CHILD_COUNT):
+    n = os.fork()
+    pid = os.getpid()
+    pid_list += [pid]
+
+if pid > 0:
+    # ONLY CHILDREN RUN HERE
+    print(f"Child #{pid} starting...")
     env = gym.make('2048-v0')
     agent = Agent(alpha=1e-5, n_actions=env.action_space.n)
-    n_games = 200
-    filename = '2048_plot.png'
+    n_games = GAME_COUNT
+    filename = '2048_plot_'+str(pid)+'.png'
     figure_file = './plots/'+filename
-
 
     best_score = env.reward_range[0]
     score_history = []
 
-    for i in range(n_games):
+    for i in range(n_games // CHILD_COUNT):
         observation = env.reset()
         done = False
         score = 0
@@ -33,14 +46,31 @@ if __name__ == '__main__':
             observation_, reward, done, info = env.step(sum(action)%4)
             score += reward
             observation = observation_
-        score_history.append(score)
-        avg_score = np.mean(score_history[-100:])
+            output = open('./outputs/'+str(pid)+'.txt', 'w')
+            output.write(score)
+            output.close()
 
-        if avg_score > best_score:
-            best_score = avg_score
-
-        print('episode ', i, 'score %.1f' % score, 'avg_score %.1f' % avg_score)
+        # print('episode ', i, 'score %.1f' % score, 'avg_score %.1f' % avg_score)
 
     x = [i+1 for i in range(n_games)]
     plot_learning_curve(x, score_history, figure_file)
+    
+    print(f"Child #{pid} stopping...")
+    
 
+
+else:
+    os.wait()
+    #ONLY PARENT RUNS HERE
+    score_history = []
+    for pid in pid_list:
+        file = open('./outputs/'+str(pid)+'.txt', 'r')
+        for line in file:
+            score_history.append(line)
+
+    best_score = 0
+    avg_score = np.mean(score_history[-100:])
+    if avg_score > best_score:
+        best_score = avg_score
+
+    print("\nBest score was ", best_score)
